9362ebb5e4582aa7fe82bab3059e1317d29683ec
Switchu <remplak@gmail.com>
2019-07-13 15:57:32 +0200

    WeatherService update, fetching data from URL added

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -1,5 +1,7 @@
 package pl.sda;
 
+import pl.sda.model.Current;
+
 /**
  * Hello world!
  *
@@ -7,8 +9,8 @@ package pl.sda;
 public class App 
 {
     public static void main( String[] args ){
-        //http://api.apixu.com/v1/current.json
-        //?key=c43efeb2af124a358ca81413191307
-        //&q=torun
+        WeatherService weatherService = new WeatherService("http://api.apixu.com/v1/current.json", "c43efeb2af124a358ca81413191307");
+        Current current = weatherService.getCityWeather("Torun");
+        System.out.println(current.getHumidity());
     }
 }
--- src/main/java/pl/sda/WeatherService.java
+++ src/main/java/pl/sda/WeatherService.java
@@ -1,37 +1,80 @@
 package pl.sda;
 
+import org.apache.commons.io.IOUtils;
+import pl.sda.model.Condition;
 import pl.sda.model.Current;
 import org.json.*;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
+import java.nio.charset.Charset;
 
 public class WeatherService {
-    private String finalURL, serviceAdress, serviceKey;
+    private String finalURL, url, apiKey;
 
     public WeatherService(String adress, String key){
-        this.serviceAdress = adress;
-        this.serviceKey = key;
-        this.finalURL = this.serviceAdress+"?key="+this.serviceKey;
+        this.url = adress;
+        this.apiKey = key;
+        this.finalURL = this.url +"?key="+this.apiKey+"&q=";
     }
 
     public Current getCityWeather(String city){
-        String cityURL = finalURL+"&q="+city;
+        String cityURL = finalURL+city;
+        try {
+            String data = IOUtils.toString(new URL(cityURL), Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         URL url = null;
         try {
             url = new URL(cityURL);
         } catch (MalformedURLException e){
             e.printStackTrace();
         }
-        Scanner scanner = null;
+        JSONTokener tokener = null;
         try {
-            scanner = new Scanner(url.openStream());
+            tokener = new JSONTokener(url.openStream());
         } catch (IOException e){
             e.printStackTrace();
         }
 
-        return new Current();
+        JSONObject o = new JSONObject(tokener);
+        JSONObject o2 = o.getJSONObject("current");
+        JSONObject o3 = o2.getJSONObject("condition");
+
+        Current current = Current.builder()
+                            .last_updated_epoch(o2.getLong("last_updated_epoch"))
+                            .last_updated(o2.getString("last_updated"))
+                            .temp_c(o2.getFloat("temp_c"))
+                            .temp_f(o2.getFloat("temp_f"))
+                            .build();
+
+        return current;
+//        return new  Current(o2.getLong("last_updated_epoch"),
+//                            o2.getString("last_updated"),
+//                            o2.getFloat("temp_c"),
+//                            o2.getFloat("temp_f"),
+//                            o2.getInt("is_day"),
+//                            new Condition(o3.getString("text"),
+//                                          o3.getString("icon"),
+//                                          o3.getInt("code")),
+//                            o2.getFloat("wind_mph"),
+//                            o2.getFloat("wind_kph"),
+//                            o2.getInt("wind_degree"),
+//                            o2.getString("wind_dir"),
+//                            o2.getFloat("pressure_mb"),
+//                            o2.getFloat("pressure_in"),
+//                            o2.getFloat("precip_mm"),
+//                            o2.getFloat("precip_in"),
+//                            o2.getInt("humidity"),
+//                            o2.getInt("cloud"),
+//                            o2.getFloat("feelslike_c"),
+//                            o2.getFloat("feelslike_f"),
+//                            o2.getFloat("vis_km"),
+//                            o2.getFloat("vis_miles"),
+//                            o2.getFloat("uv"),
+//                            o2.getFloat("gust_mph"),
+//                            o2.getFloat("gust_kph"));
     }
 }
